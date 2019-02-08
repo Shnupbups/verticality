e9ab02fa7598dc3d54cd3a5155963e83176f305b
Chloe Dawn <admin@designdevils.co.uk>
2019-02-08 12:55:17 +0000

    Make Climbable#canClimb default

--- src/main/java/space/chakat/verticality/block/ClimbableFenceBlock.java
+++ src/main/java/space/chakat/verticality/block/ClimbableFenceBlock.java
@@ -1,18 +1,10 @@
 package space.chakat.verticality.block;
 
-import net.minecraft.block.BlockState;
 import net.minecraft.block.FenceBlock;
-import net.minecraft.entity.LivingEntity;
-import net.minecraft.util.math.BlockPos;
 import space.chakat.verticality.Climbable;
 
 public class ClimbableFenceBlock extends FenceBlock implements Climbable {
   public ClimbableFenceBlock(Settings settings) {
     super(settings);
-  }
-
-  @Override
-  public boolean canClimb(LivingEntity entity, BlockState state, BlockPos position) {
-    return true;
   }
 }
